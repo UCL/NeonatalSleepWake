f5d22a91be365bb29b79e09059fb1e7ce2482661
Anastasis Georgoulas <a.georgoulas@ucl.ac.uk>
2019-07-22 14:52:11 +0100

    Allow missing values in tests, but warn

--- test_load.py
+++ test_load.py
@@ -1,5 +1,6 @@
 import glob
 import os
+import warnings
 
 import numpy as np
 import pandas as pd
@@ -25,8 +26,11 @@ def test_all_load_ok():
         # Check that we are getting one category and three boolean columns...
         assert all(d == dt for d, dt in zip(data.dtypes, [cat_type] + 3 * [np.dtype(bool)]))
         # ...with the right names...
-        assert data.columns == FIELDS[1:]
+        assert all(data.columns == FIELDS[1:])
         # ...and that we don't have any missing values from the conversion
-        assert data.notna().all(axis=None)
+        try:
+            assert data.notna().all(axis=None)
+        except AssertionError:
+            warnings.warn(f"Missing data found in {os.path.basename(filename)}")
         # Check that we haven't missed any metadata columns
         assert all(key in metadata for key in METADATA_COLUMNS)
