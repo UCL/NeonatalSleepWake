f9127aa7c719fa246f6651f64a60ab3837248d35
Anastasis Georgoulas <a.georgoulas@ucl.ac.uk>
2019-07-23 11:21:28 +0100

    Rename fixtures

--- test_experiment.py
+++ test_experiment.py
@@ -9,10 +9,10 @@ from experiment import Experiment
 
 
 @pytest.fixture(scope="module")
-def test_data():
+def sample_data():
     with open('tests/data/fixtures.yaml') as data_file:
         yaml_data = yaml.safe_load(data_file)[
-            os.path.basename("test_experiment")]
+            os.path.basename("sample_experiment")]
         yaml_meta = yaml_data.pop('meta', None)
     data = pd.DataFrame(yaml_data)
     data['Sleep_wake'] = data['Sleep_wake'].astype(
@@ -21,42 +21,42 @@ def test_data():
 
 
 @pytest.fixture(scope="module")
-def test_experiment(test_data):
-    return Experiment(*test_data)
+def sample_experiment(sample_data):
+    return Experiment(*sample_data)
 
 
-def test_metadata(test_experiment, test_data):
-    assert all(getattr(test_experiment, key) == value
-               for key, value in test_data[1].items())
+def test_metadata(sample_experiment, sample_data):
+    assert all(getattr(sample_experiment, key) == value
+               for key, value in sample_data[1].items())
 
 
-def test_number_epochs(test_experiment):
-    assert test_experiment.number_epochs() == 8
+def test_number_epochs(sample_experiment):
+    assert sample_experiment.number_epochs() == 8
 
 
-def test_count(test_experiment):
-    assert test_experiment.count("Awake") == 3
-    assert test_experiment.count("REM") == 2
-    assert test_experiment.count("nREM") == 2
-    assert test_experiment.count("Trans") == 1
+def test_count(sample_experiment):
+    assert sample_experiment.count("Awake") == 3
+    assert sample_experiment.count("REM") == 2
+    assert sample_experiment.count("nREM") == 2
+    assert sample_experiment.count("Trans") == 1
 
 
-def test_durations(test_experiment):
-    assert test_experiment.durations("Awake") == [2, 1]
-    assert test_experiment.durations("nREM") == [2]
-    assert test_experiment.durations("REM") == [1, 1]
-    assert test_experiment.durations("Trans") == [1]
+def test_durations(sample_experiment):
+    assert sample_experiment.durations("Awake") == [2, 1]
+    assert sample_experiment.durations("nREM") == [2]
+    assert sample_experiment.durations("REM") == [1, 1]
+    assert sample_experiment.durations("Trans") == [1]
 
 
-def test_count_transitions(test_experiment):
+def test_count_transitions(sample_experiment):
     # TODO Could be better split in different tests
-    assert test_experiment.count_transitions(["Awake"], ["REM"]) == 1
-    assert test_experiment.count_transitions(["REM"], ["Awake"]) == 1
+    assert sample_experiment.count_transitions(["Awake"], ["REM"]) == 1
+    assert sample_experiment.count_transitions(["REM"], ["Awake"]) == 1
     # Check no results:
-    assert test_experiment.count_transitions(["nREM"], ["REM"]) == 0
+    assert sample_experiment.count_transitions(["nREM"], ["REM"]) == 0
     # Check no symmetry:
-    assert test_experiment.count_transitions(["REM"], ["nREM"]) == 1
+    assert sample_experiment.count_transitions(["REM"], ["nREM"]) == 1
     # Multiple start states:
-    assert test_experiment.count_transitions(["Awake", "Trans"], ["REM"]) == 2
-    assert test_experiment.count_transitions(["Trans", "Awake"], ["REM"]) == 2
+    assert sample_experiment.count_transitions(["Awake", "Trans"], ["REM"]) == 2
+    assert sample_experiment.count_transitions(["Trans", "Awake"], ["REM"]) == 2
 
