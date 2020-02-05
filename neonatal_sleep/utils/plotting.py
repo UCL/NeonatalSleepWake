3fc049159af1e215a1f04e8b569e5bbd16a9a0fc
Anastasis Georgoulas <a.georgoulas@ucl.ac.uk>
2020-02-05 19:47:16 +0000

    Start with a rough version of a hypnogram

---
+++ neonatal_sleep/utils/plotting.py
@@ -0,0 +1,48 @@
+"""
+Utilities for creating plots from experimental data.
+"""
+import sys
+from operator import itemgetter
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+from ..load_file import load_file
+from ..experiment import Experiment
+
+
+states_to_num = {
+    "Awake": 0,
+    "Trans": 1,
+    "nREM": 2,
+    "REM": 3
+}
+
+
+def plot_hypnogram(exp):
+    """Plot a hypnogram corresponding to the given experiment."""
+    # FIXME Retrieve this in a better way, taking alignment into account
+    runs = exp._runs
+    times = list(runs.Start) + [runs.Stop.iloc[-1]]
+    # Convert each state to a number for plotting, according to mapping above
+    states = [states_to_num[s] for s in runs.From]
+    # Duplicate last state for better plotting and use NumPy for convenience
+    num_states = np.array(states + [states[-1]])
+    # Plot the timeseries and fill in boxes where state is "at least" sleeping
+    plt.step(times, num_states, where="post")
+    plt.fill_between(times, num_states, 1, where=num_states > 1, step="post")
+    # Set the location and label of the y-axis ticks based on the same mapping
+    # (the sorting is probably not needed)
+    labels, ticks = zip(*sorted(states_to_num.items(), key=itemgetter(1)))
+    plt.yticks(ticks=ticks, labels=labels)
+    plt.show()
+
+
+def entry_point():
+    # FIXME Make this less rough...
+    exp = Experiment(*load_file(sys.argv[1]))
+    plot_hypnogram(exp)
+
+
+if __name__ == "__main__":
+    entry_point()
