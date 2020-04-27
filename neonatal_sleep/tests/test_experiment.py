38dfc02e80c2a4859d9837018e8d1e023dc5f639
Anastasis Georgoulas <hionanthropos11@yahoo.com>
2020-04-27 11:44:16 +0100

    Add test for aligning to stimulus occurrence

--- neonatal_sleep/tests/test_experiment.py
+++ neonatal_sleep/tests/test_experiment.py
@@ -239,22 +239,38 @@ def test_alignment_first_not_recorded(sample_experiment):
     assert data[1]["State_change_from_preceding_epoch"].iloc[0] == "True"
 
 
-def test_alignment_to_first_stimulus(stimulus_experiment, stimulus_alignments):
-    correct_alignments = stimulus_alignments["Painful_stimulation_jump"]
-    stimulus_experiment.start_at_stimulus("Painful_stimulation")
-    data = stimulus_experiment.get_alignment_data()
-    assert isinstance(data, list)
-    assert len(data) == len(correct_alignments)
+def _compare_stimulus_alignments(computed_all, expected_all, stimulus):
+    assert isinstance(computed_all, list)
+    assert len(computed_all) == len(expected_all)
     # Check that the sleep states and limits are reported correctly
-    for computed, correct in zip(data, correct_alignments):
+    for computed, correct in zip(computed_all, expected_all):
         assert (computed.Sleep_wake == correct["states"]).all()
         # Epochs in index are 1-based but fixture data is 0-based
         assert computed.index[0] == correct["start_epoch"] + 1
         assert computed.index[-1] == correct["stop_epoch"] + 1
-    # Check that we each alignment starts with an occurrence of the stimulus
-    for computed in data:
+    # Check that each alignment starts with an occurrence of the stimulus
+    for computed in computed_all:
         # The returned alignments contain all values as strings
-        assert computed["Painful_stimulation_yes_no"].iloc[0] == "True"
+        assert computed[f"{stimulus}_yes_no"].iloc[0] == "True"
+
+
+def test_alignment_to_stimulus_jump(stimulus_experiment, stimulus_alignments):
+    """Check that aligning to the start of a stimulus works as expected."""
+    correct_alignments = stimulus_alignments["Painful_stimulation_jump"]
+    stimulus_experiment.start_at_stimulus("Painful_stimulation")
+    data = stimulus_experiment.get_alignment_data()
+    _compare_stimulus_alignments(data, correct_alignments,
+                                 "Painful_stimulation")
+
+
+def test_alignment_to_stimulus_first(stimulus_experiment, stimulus_alignments):
+    """Check that aligning to a stimulus occurrence works as expected."""
+    correct_alignments = stimulus_alignments["Painful_stimulation_first"]
+    stimulus_experiment.start_at_stimulus("Painful_stimulation",
+                                          observed_start=False)
+    data = stimulus_experiment.get_alignment_data()
+    _compare_stimulus_alignments(data, correct_alignments,
+                                 "Painful_stimulation")
 
 
 def test_get_run_error_no_alignment(sample_experiment):
