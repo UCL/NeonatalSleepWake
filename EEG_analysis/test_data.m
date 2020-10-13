090986f7cffbb416e1527012b64c102b82e7e453
Tuomas Koskela <t.koskela@ucl.ac.uk>
2020-10-13 15:57:12 +0100

    fix comments

--- EEG_analysis/test_data.m
+++ EEG_analysis/test_data.m
@@ -2,25 +2,25 @@ function data = test_data(N)
 
 data = struct();
 
-data.times = 1:N;
+data.srate = 1e3;
+
+data.times = 1:N; % in ms
 data.data = [ones(1,N);zeros(1,N);ones(1,N)*-1] * 1e6; % In microvolt
 
 data.event(1).type = 'T1_burst';
-data.event(1).latency = 3; % In ms
-data.event(1).duration = 10; % In ms
+data.event(1).latency = 3; % In sampling points
+data.event(1).duration = 10; % In sampling points
 
 data.event(2).type = 'T2_burst';
-data.event(2).latency = 75; % In ms
-data.event(2).duration = 10; % In ms
+data.event(2).latency = 75; % In sampling points
+data.event(2).duration = 10; % In sampling points
 
 data.event(3).type = 'T3_burst';
-data.event(3).latency = 33; % In ms
-data.event(3).duration = 10; % In ms
+data.event(3).latency = 33; % In sampling points
+data.event(3).duration = 10; % In sampling points
 
 data.chanlocs(1).labels = 'T1';
 data.chanlocs(2).labels = 'T2';
 data.chanlocs(3).labels = 'T3';
-
-data.srate = 1e3;
 
 end
