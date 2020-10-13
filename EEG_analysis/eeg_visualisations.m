d0edb817a07f0baae835cff3903142eeba5322e9
Tuomas Koskela <t.koskela@ucl.ac.uk>
2020-10-13 15:58:16 +0100

    Add GUIs for selecting variables

--- EEG_analysis/eeg_visualisations.m
+++ EEG_analysis/eeg_visualisations.m
@@ -1,14 +1,16 @@
 % Read in data
-set_filename = uigetfile('*.set','Select a .set file');
+[set_filename, pathname] = uigetfile('*.set','Select a .set file');
 if set_filename == 0
     error('No .set file selected')
 end
-eeg_data = pop_loadset(set_filename);
+eeg_data = pop_loadset([pathname,set_filename]);
 %%
+% Channel selection GUI
 channels = extractfield(eeg_data.chanlocs, 'labels');
-[index,tf] = listdlg('ListString',channels,'Name','Select channel(s)');
+[index,~] = listdlg('ListString',channels,'Name','Select channel(s)');
 channels = channels(index);
 %%
+% Process the data for plotting
 test_process_bursts
 events = process_bursts(eeg_data,channels);
 %%
@@ -21,6 +23,13 @@ plot_events(events,'power',channels);
 % Plot the events by power normalised by the duration as a function of time
 plot_events(events,'power_n',channels);
 %%
+% Field selection GUI
+fields = {'latency','duration','power','power_n'};
+[index,~] = listdlg('ListString',fields,...
+    'SelectionMode','single',...
+    'Name','Select field for frequency analysis');
+field = fields{index};
+%%
 for i = 1:numel(channels)
-    periodicity(events,channels{i},true);
+    periodicity(events,channels{i},field,true);
 end
