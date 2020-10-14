38190810de3eeddeacbaf6e2f5f30482cdc113f1
Tuomas Koskela <t.koskela@ucl.ac.uk>
2020-10-14 11:24:05 +0100

    Add loop over multiple files

--- EEG_analysis/eeg_visualisations.m
+++ EEG_analysis/eeg_visualisations.m
@@ -1,35 +1,65 @@
-% Read in data
-eeg_data = pop_loadset();
-if isempty(eeg_data)
-    error('No .set file selected')
-end
-% [] Run this script in a loop over multiple files
+% Set initial values
+field_index = 1;
+channel_index = 1;
+execute_loop = true;
 %%
-% Channel selection GUI
-channels = extractfield(eeg_data.chanlocs, 'labels');
-[index,~] = listdlg('ListString',channels,'Name','Select channel(s)');
-channels = channels(index);
-%%
-% Process the data for plotting
-test_process_bursts
-events = process_bursts(eeg_data,channels);
-%%
-% Plot the events by duration as function of time
-plot_events(events,'duration',channels);
-%%
-% Plot the events by power as function of time
-plot_events(events,'power',channels);
-%%
-% Plot the events by power normalised by the duration as a function of time
-plot_events(events,'power_n',channels);
-%%
-% Field selection GUI
-fields = {'latency','duration','power','power_n'};
-[index,~] = listdlg('ListString',fields,...
-    'SelectionMode','single',...
-    'Name','Select field for frequency analysis');
-field = fields{index};
-%%
-for i = 1:numel(channels)
-    periodicity(events,channels{i},field,true);
+while execute_loop
+    %%
+    % Read in data
+    eeg_data = pop_loadset();
+    if isempty(eeg_data)
+        error('No .set file selected')
+    end
+    %%
+    % Channel selection GUI
+    channels = extractfield(eeg_data.chanlocs, 'labels');
+    [channel_index,~] = listdlg('ListString',channels,...
+        'PromptString','Select channel(s)',...
+        'InitialValue',channel_index);
+    channels = channels(channel_index);
+    %%
+    % Process the data for plotting. Do it only once and only for the requested
+    % channels to avoid unnecessary work.
+    test_process_bursts % Test power integration is correct
+    events = process_bursts(eeg_data,channels);
+    %%
+    % Plot the events by duration as function of time
+    plot_events(events,'duration',channels);
+    %%
+    % Plot the events by power as function of time
+    plot_events(events,'power',channels);
+    %%
+    % Plot the events by power normalised by the duration as a function of time
+    plot_events(events,'power_n',channels);
+    %%
+    % Field selection GUI
+    fields = {'none','latency','duration','power','power_n'};
+    [field_index,tf] = listdlg('ListString',fields,...
+        'SelectionMode','single',...
+        'PromptString','Select field for frequency analysis',...
+        'ListSize',[300,300],...
+        'InitialValue',field_index);
+    if tf == 0 || field_index == 1
+        field = [];
+    else
+        field = fields{field_index};
+    end
+    %%
+    % Plot frequency analysis for selected field&channels
+    if ~isempty(field)
+        for i = 1:numel(channels)
+            periodicity(events,channels{i},field,true);
+        end
+    end
+    %%
+    % GUI to exit the loop
+    % Exit unless the user clicks on 'Yes'
+    answer = questdlg('read another file?');
+    switch answer
+        case 'Yes'
+            execute_loop = true;
+        otherwise
+            execute_loop = false;
+    end
+    %%
 end
