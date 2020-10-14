cec7ac74388fb75871d477c9a47886cdad9231b7
Tuomas Koskela <t.koskela@ucl.ac.uk>
2020-10-14 12:36:39 +0100

    Add comments, use format_channels function

--- EEG_analysis/plot_events.m
+++ EEG_analysis/plot_events.m
@@ -2,26 +2,23 @@ function fig = plot_events(events, field, channels_in)
 % function fig = plot_events(events, field, channels_in)
 %
 % Plot burst durations by type of burst with latency on the x-axis
-% Arguments
-% events : Struct with events by channel. Produced by process_bursts.m or
+% Inputs:
+%    - events: Struct with events by channel. Produced by process_bursts.m or
 %          get_events_by_channel.m
-% field : quantity to plot, supported options are
-%         - 'duration'
-%         - 'power'
-%         - 'power_n'
-% channels_in : string or cell array of channel names
+%    - field: quantity to plot, supported options are
+%            - 'duration'
+%            - 'power'
+%            - 'power_n'
+%    - channels_in: string or cell array of channel names (usually found in
+%                   eeg_data.chanlocs.labels)
+% Outputs:
+%    - fig: figure handle
 %
 % EXAMPLES:
 % plot_events(set_filename,'duration','O2');
 % plot_events(set_filename,'power',{'O2','C4'});
 
-if iscell(channels_in)
-    channels = channels_in;    
-elseif ischar(channels_in) || isstring(channels_in)
-    channels = {channels_in};
-else
-    error('Types must be a string or a cell array');
-end
+channels = format_channels(channels_in);
 
 fig = figure();
 clf;
--- EEG_analysis/process_bursts.m
+++ EEG_analysis/process_bursts.m
@@ -1,29 +1,43 @@
 function events = process_bursts(eeg_data, channels_in, events_in)
-   
+% function events = process_bursts(eeg_data, channels_in, events_in)
+%
+% Separates burst events in eeg data by channel, calculates integrated
+% power for each event.
+% Inputs:
+%    - eeg_data: data from eeglab .set file
+%    - channels_in: string or cell array of channel names (usually found in
+%                   eeg_data.chanlocs.labels)
+%    - events_in: (optional) events previously processed by this function
+%                 (to avoid reprocessing channels)
+% Outputs:
+%    - events: struct with substructs for each channel. Each channel struct
+%              has the fields 'latency' and 'duration'. In addition,
+%              processed channels have the fields 'power' and 'power_n'
+
+
+% Threshold number of events to display warning and progress bar
 lots_of_events = 2e3;
 
+% Handle optional argument
 if nargin < 3
     events = get_events_by_channel(eeg_data);
 else
     events = events_in;    
 end
 
-if iscell(channels_in)
-    channels = channels_in;    
-elseif ischar(channels_in) || isstring(channels_in)
-    channels = {channels_in};
-else
-    error('Channels must be a string or a cell array');
-end
+channels = format_channels(channels_in);
 
 for ic = 1:numel(channels)
     channel = channels{ic};
 
-    % Check if this channel has already been processed
+    % Skip this channel if it has already been processed
     if (isfield(events.(channel),'power') && isfield(events.(channel), 'power_n'))
         continue
     end        
     
+    % If we are processing a large number of events, this function can be
+    % slow. In that case, give the user a friendly warning and display a
+    % progress bar.
     if events.(channel).n > lots_of_events
         warning(['computing the power in ',num2str(events.(channel).n),...
             ' events. This may take a while.'])
@@ -41,20 +55,21 @@ for ic = 1:numel(channels)
     
     events.(channel).power = zeros(1,events.(channel).n);
     
+    % Integrate power in each event
     for i = 1:events.(channel).n
-        try
-            i1 = int64(events.(channel).latency(i) * eeg_data.srate);
-            i2 = int64((events.(channel).latency(i) + events.(channel).duration(i)) * eeg_data.srate);
-            events.(channel).power(i) = trapz(time_series(i1:i2), power_series(i1:i2));
-        catch
-            keyboard
-        end
+        % Find event start and end indices
+        i1 = int64(events.(channel).latency(i) * eeg_data.srate);
+        i2 = int64((events.(channel).latency(i) + events.(channel).duration(i)) * eeg_data.srate);
+        % Approximate integral with the trapezoid rule
+        events.(channel).power(i) = trapz(time_series(i1:i2), power_series(i1:i2));
+        
         if wb_exists
             waitbar(i/events.(channel).n, wb, ['Processing events for channel ',channel])
         end
     end
     events.(channel).unit_power = '\muV^2';
     
+    % Power normalized by event duration
     events.(channel).power_n = events.(channel).power ./ events.(channel).duration;
     events.(channel).unit_power_n = '\muV^2s^{-1}';
     
