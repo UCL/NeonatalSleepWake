d51fceb44df774719b32300080555302deb824ce
Tuomas Koskela <t.koskela@ucl.ac.uk>
2020-10-15 15:08:59 +0100

    output the max amplitude and corresponding period

--- EEG_analysis/periodicity.m
+++ EEG_analysis/periodicity.m
@@ -1,4 +1,4 @@
-function periodicity(events, channel, field, smooth, color)
+function [period,amplitude] = periodicity(events, channel, field, smooth, color)
 % function periodicity(events, channel, field, smooth, color)
 %
 % Perform and plot frequency analysis for events in a selected channel.
@@ -51,9 +51,10 @@ xlabel('Time (hrs)')
 ylabel(yl)
 title(['Signal for channel ', channel])
 
+% Approximate frequency of events
 Fs = numel(t)/t_uniform(end);
 
-% Copied from 'doc fft'
+% Do fft over the signal. Code is from 'doc fft'.
 fty = fft(y_uniform);
 P2 = abs(fty/L);
 P1 = P2(1:L/2+1);
@@ -68,8 +69,14 @@ ylabel('Amplitude')
 title(['Fourier spectrum for channel ', channel])
 grid on
 
+% Continuous wavelet analysis. Just use what comes out of the box.
 figure()
 cwt(y_uniform, hours(1/Fs/3600))
 title([get(gca,'title').String, ' for channel ',channel])
+
+% Find the period with the highest amplitude. Ignore the lowest
+% frequency in  the spectrum (f == 0.)
+[amplitude, imax] = max(P1(2:end));
+period = 1./f(imax+1);
 
 end
