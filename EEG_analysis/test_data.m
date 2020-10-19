d5db9e0434b0e58ca03a078daf442900245f51d0
Tuomas Koskela <t.koskela@ucl.ac.uk>
2020-10-19 12:54:41 +0100

    Added test for remove_duplicate_events

--- EEG_analysis/test_data.m
+++ EEG_analysis/test_data.m
@@ -5,7 +5,7 @@ data = struct();
 data.srate = 1e3;
 
 data.times = 1:N; % in ms
-data.data = [ones(1,N);zeros(1,N);ones(1,N)*-1] * 1e6; % In microvolt
+data.data = [ones(1,N);zeros(1,N);ones(1,N)*-1;ones(1,N)] * 1e6; % In microvolt
 
 data.event(1).type = 'T1_burst';
 data.event(1).latency = 3; % In sampling points
@@ -19,8 +19,17 @@ data.event(3).type = 'T3_burst';
 data.event(3).latency = 33; % In sampling points
 data.event(3).duration = 10; % In sampling points
 
+data.event(4).type = 'T4_burst';
+data.event(4).latency = 33; % In sampling points
+data.event(4).duration = 10; % In sampling points
+
+data.event(5).type = 'T4_burst';
+data.event(5).latency = 33; % In sampling points
+data.event(5).duration = 7; % In sampling points
+
 data.chanlocs(1).labels = 'T1';
 data.chanlocs(2).labels = 'T2';
 data.chanlocs(3).labels = 'T3';
+data.chanlocs(4).labels = 'T4';
 
 end
