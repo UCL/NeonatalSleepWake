268603978abcf8eb807ab43a706954b66ac95ec0
Tuomas Koskela <t.koskela@ucl.ac.uk>
2020-12-03 15:05:53 +0000

    Add flexibility to parsing the dataset identifiers from eeglab and the corresponding spreadsheets

--- EEG_analysis/development_outcome_correlation.m
+++ EEG_analysis/development_outcome_correlation.m
@@ -6,11 +6,16 @@
 
 %% Read excel table
 [filename,pathname] = uigetfile('*.xls*');
-full_table = readtable([pathname,filename], 'UseExcel', true);
-outcome_table = full_table(:,{'BRUK','Shankaran_num','Cog','Lang_interp','Motor_interpolated'});
 %% Read data from eeg_visualisations
 uiopen('load');
 %% Add columns for period and amplitude to the table
+full_table = readtable([pathname,filename], 'UseExcel', true);
+
+prefix = extractfield(eeg_periodicity_data,'id_prefix');
+unique_prefix = unique(prefix);
+assert(numel(unique_prefix) == 1, 'Multiple id prefixes found');
+outcome_table = full_table(:,{unique_prefix{1},...
+    'Shankaran_num','Cog','Lang_interp','Motor_interpolated'});
 n = size(outcome_table,1);
 % Find unique channel names
 c = extractfield(eeg_periodicity_data,'channels');
@@ -29,6 +34,7 @@ end
 
 for i = 1:numel(eeg_periodicity_data)
     data = eeg_periodicity_data(i);
+    assert(isfinite(data.id), ['Could not find BRUK value in data set ',data.setname])
     for j = 1:data.nchannels
         channel = data.channels{j};
         field_p = [channel,'_period'];
@@ -37,14 +43,14 @@ for i = 1:numel(eeg_periodicity_data)
         field_pcoeff = [channel,'_p_powerN_sparsity'];
         field_pmean = [channel,'_mean_powerN'];
         field_pmedian = [channel,'_median_powerN'];
-        outcome_table.(field_p)(outcome_table.BRUK == data.BRUK) = hours(data.period(j));
-        outcome_table.(field_a)(outcome_table.BRUK == data.BRUK) = data.amplitude(j);
-        outcome_table.(field_rcoeff)(outcome_table.BRUK == data.BRUK) = data.corrcoef_r(j);
-        outcome_table.(field_pcoeff)(outcome_table.BRUK == data.BRUK) = data.corrcoef_p(j);
-        outcome_table.(field_pmean)(outcome_table.BRUK == data.BRUK) = data.mean_power(j);
-        outcome_table.(field_pmedian)(outcome_table.BRUK == data.BRUK) = data.median_power(j);
+        outcome_table.(field_p)(outcome_table.(unique_prefix{1}) == data.id) = hours(data.period(j));
+        outcome_table.(field_a)(outcome_table.(unique_prefix{1}) == data.id) = data.amplitude(j);
+        outcome_table.(field_rcoeff)(outcome_table.(unique_prefix{1}) == data.id) = data.corrcoef_r(j);
+        outcome_table.(field_pcoeff)(outcome_table.(unique_prefix{1}) == data.id) = data.corrcoef_p(j);
+        outcome_table.(field_pmean)(outcome_table.(unique_prefix{1}) == data.id) = data.mean_power(j);
+        outcome_table.(field_pmedian)(outcome_table.(unique_prefix{1}) == data.id) = data.median_power(j);
     end
-    outcome_table.deleted_fraction(outcome_table.BRUK == data.BRUK) = data.deleted_fraction;
+    outcome_table.deleted_fraction(outcome_table.(unique_prefix{1}) == data.id) = data.deleted_fraction;
 end
 %% TODO: Get correlation coefficients
 % [ ] Ignore NaN's with corrcoef(X,Y,'rows','complete')
