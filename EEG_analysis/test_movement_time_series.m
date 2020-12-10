2477f47fc1499635cef7c06fdcc1df8769b97976
Tuomas Koskela <t.koskela@ucl.ac.uk>
2020-12-10 15:34:07 +0000

    Add tests for statistics

--- EEG_analysis/test_movement_time_series.m
+++ EEG_analysis/test_movement_time_series.m
@@ -19,8 +19,25 @@ params.dt = mean(diff(data.Time), 'omitnan');
 
 light_events = detect_light_events(data, params);
 movement_events = detect_movement_events(data, params, light_events);
+stats = movement_event_statistics(data, movement_events, false);
 
-assert(isempty(light_events.onset), ['Expected no light events, got ' num2str(numel(light_events.onset))]);
-assert(movement_events.y1.n_events == 2, ['Expected 2 events in y1, got ' num2str(movement_events.y1.n_events)]);
-assert(movement_events.y2.n_events == 4, ['Expected 4 events in y2, got ' num2str(movement_events.y2.n_events)]);
-assert(movement_events.y3.n_events == 6, ['Expected 6 events in y3, got ' num2str(movement_events.y3.n_events)]);
+reference_n_events = [NaN,2,4,6];
+
+assert(isempty(light_events.onset), ...
+    ['Expected no light events, got ' num2str(numel(light_events.onset))]);
+
+for i = 2:4
+    y = varnames{i};
+    n = reference_n_events(i);
+    ns = num2str(n);
+    assert(movement_events.(y).n_events == n, ...
+        ['Expected ' ns ' events in ' y ', got ' num2str(movement_events.(y).n_events)]);
+    assert(all(stats.(y).median < 1), ...
+        ['Expected median of ' y ' < 1, got ', num2str(max(stats.(y).median))])
+    assert(all(stats.(y).mean < 1), ...
+        ['Expected mean of ' y ' < 1, got ', num2str(max(stats.(y).median))])
+    assert(all(stats.(y).p25 < stats.(y).median), ...
+        ['Expected 25th percentile < median of ' y])
+    assert(all(stats.(y).p75 > stats.(y).median), ...
+        ['Expected 75th percentile > median of ' y])
+end
