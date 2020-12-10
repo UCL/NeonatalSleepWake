a1edb0f0f0ab8b768386705f6e7bf2dbcbc61176
Tuomas Koskela <t.koskela@ucl.ac.uk>
2020-12-10 15:48:11 +0000

    skip detection if only one signal

--- EEG_analysis/detect_light_events.m
+++ EEG_analysis/detect_light_events.m
@@ -5,8 +5,10 @@ time = variables(:,1);
 data = variables(:,2:end);
 light_threshold = mean(data, 1, 'omitnan') + params.light_threshold_std * std(data, 1, 'omitnan');
 light_mask = false(size(time));
-for i = 1:numel(time)
-    light_mask(i) = all(data(i,:) > light_threshold);
+if size(data,2) > 1
+    for i = 1:numel(time)
+        light_mask(i) = all(data(i,:) > light_threshold);
+    end
 end
 light_events = struct();
 light_events.onset = find(light_mask);
