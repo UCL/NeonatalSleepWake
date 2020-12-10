9134e18d10964d86080a146bc6d31cb8d14ab8f1
Tuomas Koskela <t.koskela@ucl.ac.uk>
2020-12-10 14:41:55 +0000

    Reset default parameters, update comments

--- EEG_analysis/movement_time_series.m
+++ EEG_analysis/movement_time_series.m
@@ -1,10 +1,6 @@
 % This script processes time series that contain pixel change rates
-% processed from video clips. It detects movement events by the following
-% criterion:
-% - signal above 3 standard deviations of the mean with gaps of less than
-%   1000ms below threshold in between 
-% In addition, events that occurr simultaneously in each time series and
-% are above 5 standard deviations
+% processed from video clips. It detects movement events and light events,
+% plots them, and displays statistical information about detected events.
 
 %% Run tests
 runtests('test_movement_time_series.m')
@@ -16,8 +12,8 @@ params = struct();
 params.movement_threshold_std = 3;
 params.light_threshold_std = 5;
 params.duration_limit = 40 * 1e-3;
-params.interval_limit = 100 * 1e-3;
-params.period_after_end = 50 * 1e-3;
+params.interval_limit = 1000 * 1e-3;
+params.period_after_end = 500 * 1e-3;
 params.dt = mean(diff(data_table.Time), 'omitnan');
 %% Detect light events
 light_events = detect_light_events(data_table, params);
