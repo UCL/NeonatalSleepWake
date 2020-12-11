fa5ed749246d5e34fcaf93ce162933b38685c02c
Tuomas Koskela <t.koskela@ucl.ac.uk>
2020-12-11 10:38:24 +0000

    Add docstring

--- EEG_analysis/detect_light_events.m
+++ EEG_analysis/detect_light_events.m
@@ -1,4 +1,18 @@
 function light_events = detect_light_events(data_table, params)
+% function light_events = detect_light_events(data_table, params)
+%
+% Detect light events from the data in data_table, by assuming that an
+% event with params.light_threshold_std * the standard deviation above the
+% mean of the signal which occurs simultaneously in all signals is a light
+% event.
+%
+% Inputs:
+%   - data_table: table with Time in the first column and 2 or more signals
+%                 in the columns 2:end
+%   - params: struct with the field ligth_threshold_std set.
+% Outputs:
+%   - light_events: struct with the onset (in frames) of detected light
+%                   events
 
 variables = data_table.Variables;
 time = variables(:,1);
