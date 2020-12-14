0bdaed1fdc9f67264d9bb010a30d12c5f9ff795d
Tuomas Koskela <t.koskela@ucl.ac.uk>
2020-12-14 11:37:53 +0000

    Add option to normalize event duration

--- EEG_analysis/movement_event_statistics.m
+++ EEG_analysis/movement_event_statistics.m
@@ -12,6 +12,7 @@ function stats = movement_event_statistics(data_table, events, varargin)
 %   Optional:
 %     - visualize (default true): plot the result
 %     - baseline (default 1.0): period of time added before the event onset
+%     - normalize (default false): normalize all events to unit duration
 % Outputs:
 %   - stats: struct with the analyzed event signals and the computed
 %            statistics
@@ -36,12 +37,21 @@ for iname = 2:numel(variable_names)
     signal = NaN(max_duration,events.(varname).n_events);
 
     data = data_table.(varname);
-    
+
     for ievent = 1:events.(varname).n_events
         ibeg = max(1, events.(varname).onset(ievent)-baseline_frames);
         iend = min(numel(data), events.(varname).offset(ievent));
         signal(1:iend-ibeg+1,ievent) = data(ibeg:iend);
-    
+
+        if params.Results.normalize
+
+            x = baseline_frames:iend-ibeg+1;
+            v = signal(baseline_frames:iend-ibeg+1,ievent);
+            xq = linspace(baseline_frames,x(end),max_duration-baseline_frames);
+            signal(baseline_frames+1:end,ievent) = interp1(x,v,xq,'linear');
+
+        end
+
     end
 
     stats.(varname).signal = signal;
@@ -49,16 +59,23 @@ for iname = 2:numel(variable_names)
     stats.(varname).mean = mean(signal, 2, 'omitnan');
     stats.(varname).p25 = prctile(signal,25,2);
     stats.(varname).p75 = prctile(signal,75,2);
+    stats.(varname).stderr = std(signal, 0, 2, 'omitnan') / sqrt(events.(varname).n_events);
 
     if params.Results.visualize
         subplot(numel(variable_names)-1, 1, iname-1);
-        plot(t,signal, 'b')
+        %plot(t,signal, 'b')
         hold on
         plot(t,stats.(varname).median, 'r', 'linewidth',2)
-        %plot(t,stats.(varname).p25, 'r--', 'linewidth', 2)
-        %plot(t,stats.(varname).p75, 'r--', 'linewidth', 2)
+        fill([t,fliplr(t)],...
+            [stats.(varname).median - stats.(varname).stderr; ...
+            flipud(stats.(varname).median + stats.(varname).stderr)], ...
+            'r','facealpha',0.25,'edgecolor','r')
         title(varname)
-        xlabel('time (s)')
+        if params.Results.normalize
+            xlabel('duration (normalized)')
+        else
+            xlabel('time (s)')
+        end
         ylabel('pixel change')
     end
 end
